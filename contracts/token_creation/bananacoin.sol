f2ef63af1dff823eb34b09e6b7bb0802fc8f15a0
joao <veigajoaoa@gmail.com>
2022-01-17 11:08:35 -0300

    add erc20 token

---
+++ contracts/token_creation/bananacoin.sol
@@ -0,0 +1,13 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BANCOINtoken is ERC20("BananaCoin", "BANCOIN") {
+
+    constructor() {
+        _mint(msg.sender, 5.8 * 10**6);
+    }
+
+}
