a8f2421f1b50ae5a1f8522a7ea535dbee87478e2
joao <veigajoaoa@gmail.com>
2022-02-22 09:28:28 -0300

    build utilities

--- deployment/transaction_utilities/send_bnb_to_accounts.js
+++ deployment/transaction_utilities/send_bnb_to_accounts.js
@@ -2,6 +2,6 @@ import {deploy, web3, provider} from '../_deploy_script.js';
 
 const accounts = await web3.eth.getAccounts();
 
-web3.eth.sendTransaction({to:accounts[10], from:accounts[0], value:web3.utils.toWei("0.01", "ether")})
+web3.eth.sendTransaction({to:accounts[10], from:accounts[0], value:web3.utils.toWei("0.0001", "ether")})
 
 provider.engine.stop();
--- deployment/transaction_utilities/send_tokens_early_investors.js
+++ deployment/transaction_utilities/send_tokens_early_investors.js
@@ -7,7 +7,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const contractPathERC20 = path.resolve(__dirname, '../bin/contracts/token_creation', 'BananaCoin.json');
+const contractPathERC20 = path.resolve(__dirname, '../../bin/contracts/token_creation', 'BananaCoin.json');
 const compiledERC20 = JSON.parse(fs.readFileSync(contractPathERC20, 'utf8'));
 
 const contractInstance = new web3.eth.Contract(
@@ -18,6 +18,6 @@ const contractInstance = new web3.eth.Contract(
 import prompt from 'prompt-sync';
 const investor_account = prompt()('investor wallet address: ');
 
-await contractInstance.methods.transfer(investor_account, web3.utils.toWei("3080", "ether")).send({ gas: '1000000', from: accounts[10] });
+await contractInstance.methods.transfer(investor_account, web3.utils.toWei("2330", "ether")).send({ gas: '1000000', from: accounts[10] });
 
 provider.engine.stop();
